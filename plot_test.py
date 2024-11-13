fdce1102868b5278d77212a5cbbe4731308316ab
Baleninha <61008808+baleninha@users.noreply.github.com>
2024-11-13 22:45:03 +0100

    Update plot_test.py

--- plot_test.py
+++ plot_test.py
@@ -5,19 +5,18 @@ import numpy as np
 
 data = pd.read_csv(r'C:\Users\baleninha\Downloads\httpsearthquake_usgs_govearthquakesmap.csv')
 
-# Filtramos los datos esenciales
+# filtrar los datos relevantes
 df = data[['latitude', 'longitude', 'mag', 'place']]
 
-# Crear el scatter_mapbox
 fig = px.scatter_mapbox(
     df,
     lat="latitude",
     lon="longitude",
     hover_name="place",
     hover_data={"mag": True},
-    color="mag",  # Usamos magnitud para colorear los puntos
-    size="mag",   # Usamos magnitud para el tamaño del punto
-    size_max=15,  # Escalamos el tamaño al doble
+    color="mag",  # usamos magnitud para colorear los puntos
+    size="mag",   # usamos magnitud para el tamaño del punto
+    size_max=15,  # escalamos el tamaño al doble
     color_continuous_scale="Viridis",
     mapbox_style="open-street-map",
     zoom=1,
@@ -28,17 +27,16 @@ fig = px.scatter_mapbox(
 ##########
 
 df = data[['latitude', 'longitude', 'mag', 'place']].copy()
-df['scaled_size'] = df['mag'] ** 5 # Ajusta el factor multiplicativo según el tamaño que desees
+df['scaled_size'] = df['mag'] ** 5 
 
-# Crear el scatter_mapbox con tamaño ajustado
 fig = px.scatter_mapbox(
     df,
     lat="latitude",
     lon="longitude",
     hover_name="place",
     hover_data={"mag": True},
-    color="mag",  # Colorear los puntos según la magnitud
-    size= "scaled_size",  # Usar la columna escalada para el tamaño
+    color="mag",  
+    size= "scaled_size",  
     color_continuous_scale="Viridis",
     mapbox_style="open-street-map",
     zoom=1,
@@ -61,5 +59,4 @@ fig.update_layout(
 fig.update_layout(title="Mapa de Terremotos", title_x=0.5)
 fig.show()
 
-# Guardar como archivo HTML y abrir en navegador
 fig.write_html("./static_web/mapa_terremotos.html")
